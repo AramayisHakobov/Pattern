5d9d18b3d3b85b1186434dc62e99dbacac05c6a8
Aramayis Hakobov <87491291+AramayisHakobov@users.noreply.github.com>
2023-09-30 22:32:39 +0400

    *

--- src/Pattern_15.java
+++ src/Pattern_15.java
@@ -10,7 +10,7 @@ public class Pattern_15 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                if (j == i || j == 1||i==5) {
+                if (j == i || j == 1 || i==5) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
