5a4e25934496ce31daf5a777f3a17ccb12236958
Aramayis Hakobov <87491291+AramayisHakobov@users.noreply.github.com>
2023-09-30 22:20:20 +0400

    *

---
+++ src/Pattern_17.java
@@ -0,0 +1,24 @@
+public class Pattern_17 {
+    /**
+     *****
+     *  *
+     * *
+     **
+     *
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                if (i == 1 || j == i || j == 5)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern_18.java
@@ -0,0 +1,23 @@
+public class Pattern_18 {
+    /**
+     *****
+      *  *
+       * *
+        **
+         *
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k >= i; k--) {
+                if (i == 1 || k == i || k == 5)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
